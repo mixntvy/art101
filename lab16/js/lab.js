58bd39dc08a1beaf768a7610eda40610b823dc3f
Mint <mixntvy@gmail.com>
2025-06-05 13:11:17 -0700

    lab 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -7,31 +7,36 @@
 
 //ajax object
 const ajaxObj ={
-  url: "https://xkcd.com/info.0.json",
-  data: {
-    api_key: "ftYwJofhE3Deu3fWhjU7q3RyJo4RgHkO3MW0tGYW",
-    count: 1
-  },
+    // asked chat gpt for a bit of help with the CORS error and suggested this edit to the url line
+  url: "https://api.allorigins.win/get?url=" + encodeURIComponent("https://xkcd.com/info.0.json"),
+//   data: {
+//     api_key: "ftYwJofhE3Deu3fWhjU7q3RyJo4RgHkO3MW0tGYW",
+//     count: 1
+//   },
   type: "GET",
   dataType: "JSON"
   
 }
 
 //button listener 
-$("button").click(function(){
+$("#action").click(function(){
   
   //call ajax
   $.ajax(ajaxObj)
 
   //success callback 
-  .done(function(data){
+  .done(function(response){
+    //asked chat gpt for a bit of help with the CORS error i was getting in the 
+    // console and it suggested i add this line 
+    const data = JSON.parse(response.contents);
+
     console.log(data);
   
     //pull out relevant data from results 
-    let title = data[0].title;
-    let date = data[0].date;
-    let explanation = data[0].explanation;
-    let imageURL = data[0].url;
+    let title = data.title;
+    let date = `${data.year}-${data.month}-${data.day}`;
+    let explanation = data.transcript;
+    let imageURL = data.img;
     
     //put data in output
     $("#output").html(`<h2>${title}</h2>`);
